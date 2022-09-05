266d0d3753ae9f2c5f6a3f822beaf71b1bed7476
Marclemer <marc-lemer@hotmail.com>
2022-09-05 15:17:11 +0200

    Création du user-model+dossier vide du controllers et validator

---
+++ models/user-model.js
@@ -0,0 +1,45 @@
+const{Schema,model}=require('mongoose');
+
+//Création d'un schema par défaut
+
+const userSchema=new Schema({
+    firstname:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    lastname:{
+        type:String,
+        require:true,
+        trim:true},
+    email:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        require:true
+    },
+    pays:{
+        type:String,
+        require:true,
+    },
+    telephone:{
+        type:Number,
+        require:true
+    },
+    role:{
+        type:String,
+        enum:["User","Admin"],
+        require:true,
+        default:'User'
+    }
+},{
+    collection:"User",
+    timestamps:true
+});
+//model User à partir du userSchema
+const User=model('User,userSchema');
+
+module.exports.User=User;
